b4ee9f584b108fcd869991b1dd52fbe712449eec
nani3013 <undrunarendra3013@gmai.com>
2024-11-23 10:19:09 +0530

    terraform-aws-eks

--- 40-eks/data.tf
+++ 40-eks/data.tf
@@ -1,7 +1,12 @@
 
-data "aws_ssm_parameter" "bastion_sg_id" {
+data "aws_ssm_parameter" "eks_control_plane_sg_id" {
   #/expense/dev/bastion_sg_id
-  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+  name = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
+}
+
+data "aws_ssm_parameter" "node_sg_id" {
+  #/expense/dev/bastion_sg_id
+  name = "/${var.project_name}/${var.environment}/node_sg_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
@@ -9,20 +14,10 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "eks_control_plane_sg_id" {
-  #/expense/dev/public_subnet_ids
-  name = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project_name}/${var.environment}/vpc_id"
 }
 
-data "aws_ssm_parameter" "node_sg_id" {
-  #/expense/dev/public_subnet_ids
-  name = "/${var.project_name}/${var.environment}/node_sg_id"
-}
-
-resource "aws_ssm_parameter" "vpc_id" {
-  name  = "/${var.project_name}/${var.environment}/vpc_id"
- 
-}
 data "aws_ami" "joindevops" {
 
 	most_recent      = true
